0c9c6676a6f64bf85fd28c7754b9c08d28d6eff4
Joao Silva <joaosilvaa.work@gmail.com>
2025-04-03 23:52:22 +0100

    add Ex 6

---
+++ PlayerStats/Player.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace PlayerStats
+{
+    public class Player
+    {
+    private float highScore;
+    public int PlayedGames { get; private set; }
+    public int WonGames { get; private set; }
+    public string Name { get; }
+
+    public float HighScore
+    {
+        get => highScore;
+        set
+        {
+            if (value > highScore)
+            {
+                highScore = value;
+            }
+        }
+    }
+
+    public float WinRate
+    {
+        get
+        {
+            if (PlayedGames == 0) return 0;
+            return (float)WonGames / PlayedGames;
+        }
+    }
+
+    public Player(string name)
+    {
+        Name = name;
+        highScore = 0;
+        PlayedGames = 0;
+        WonGames = 0;
+    }
+
+    public void PlayGame(bool win)
+    {
+        PlayedGames++;
+        if (win)
+        {
+            WonGames++;
+        }
+    }
+
+    }
+}
