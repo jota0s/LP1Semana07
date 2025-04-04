0e74dbccc7d4ad8d88b20e5e6e10e9f9892233c2
Joao Silva <joaosilvaa.work@gmail.com>
2025-04-04 14:33:56 +0100

    add MyRPG

---
+++ MyRPG/MyRPG.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyRPG
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello LP1!");
+        }
+    }
+}
